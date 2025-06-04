913260101baaaef6dfc9536d0ec87673fed6a67e
Longos26 <romarlongos026@gmail.com>
2025-06-05 00:17:06 +0800

    latest

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -243,7 +243,7 @@ export default function UpdatePost() {
   return (
     <div className="max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
       <h1 className="text-4xl font-bold text-center mb-10 text-gray-900 dark:text-white">
-        Update Post
+        Update Content
       </h1>
 
       {publishError && (
